22265093acc7bea9a60e802949eeb32e46fef363
Ilya <vid1525@yandex.ru>
2020-11-17 22:37:55 +0300

    hw6

--- 06/Format.h
+++ 06/Format.h
@@ -12,25 +12,25 @@ private:
 public:
     FormatException(const std::string &errorMsg) : errorType(errorMsg) {}
 
-    const char *what() const noexcept {
+    const char *what() const noexcept override {
         return errorType.c_str();
     }
 };
 
 template <typename CurArg>
-void append(std::vector<std::string> &vectorArgs, CurArg currentArg) {
+void append(std::vector<std::string> &vectorArgs, const CurArg &currentArg) {
     std::stringstream str;
     str << currentArg;
-    vectorArgs.push_back(std::string(str.str()));
+    vectorArgs.push_back(str.str());
 }
 
 template <typename CurArg, typename... Args>
-void makeVector(std::vector<std::string> &vectorArgs, CurArg &&currentArg) {
+void makeVector(std::vector<std::string> &vectorArgs, const CurArg &currentArg) {
     append(vectorArgs, currentArg);
 }
 
 template <typename CurArg, typename... Args>
-void makeVector(std::vector<std::string> &vectorArgs, CurArg &&currentArg, Args&&... args) {
+void makeVector(std::vector<std::string> &vectorArgs, const CurArg &currentArg, Args&&... args) {
     append(vectorArgs, currentArg);
     makeVector(vectorArgs, std::forward<Args>(args)...);
 }
@@ -41,10 +41,10 @@ std::string format(const std::string &formatString, Args&&... args) {
     makeVector(vectorArgs, std::forward<Args>(args)...);
 
     std::string answer;
-    int64_t len = static_cast<int64_t>(formatString.size());
-    for (int64_t i = 0; i < len; ) {
+    size_t len = static_cast<int64_t>(formatString.size());
+    for (size_t i = 0; i < len; ) {
         if (formatString[i] == '{') {
-            int64_t j = i + 1;
+            size_t j = i + 1;
             while (j < len && formatString[j] != '}' && formatString[j] != '{') {
                 ++j;
             }
@@ -52,13 +52,13 @@ std::string format(const std::string &formatString, Args&&... args) {
             if (j == len) {
                 std::stringstream str;
                 str << "No closing bracket in the end\n";
-                throw FormatException(std::string(str.str()));
+                throw FormatException(str.str());
             }
 
             if (formatString[j] == '{') {
                 std::stringstream str;
                 str << "Doubled open bracket index: " << j << "\n";
-                throw FormatException(std::string(str.str()));
+                throw FormatException(str.str());
             }
 
             int64_t index;
@@ -67,13 +67,13 @@ std::string format(const std::string &formatString, Args&&... args) {
             } catch (const std::exception &invalid_value) {
                 std::stringstream str;
                 str << "Invalid argument\nindex: [" << i << ".." << j << "]\n";
-                throw FormatException(std::string(str.str()));
+                throw FormatException(str.str());
             }
 
             if (index < 0 || index >= static_cast<int64_t>(vectorArgs.size())) {
                 std::stringstream str;
                 str << "Index out of range index: " << index << "\n";
-                throw FormatException(std::string(str.str()));
+                throw FormatException(str.str());
             }
 
             answer += vectorArgs[index];
@@ -81,7 +81,7 @@ std::string format(const std::string &formatString, Args&&... args) {
         } else if (formatString[i] == '}') {
             std::stringstream str;
             str << "Invalid } index: " << i << "\n";
-            throw FormatException(std::string(str.str()));
+            throw FormatException(str.str());
         } else {
             answer += formatString[i];
             ++i;
